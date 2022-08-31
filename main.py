e295747e9b38805efcfe994604c66d8a2aa4c8bd
KT2001 <sc.talwar2001@gmail.com>
2022-08-31 22:05:48 +0530

    Third Commit

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ import pandas as pd
 #from flasgger import Swagger
 import streamlit as st 
 
-pickle_in = open("model.pkl","rb")
-classifier=pickle.load(pickle_in)
+pickle_mod = open("model.pkl","rb")
+pickle_trans = open("transformer.pkl", "rb")
+classifier=pickle.load(pickle_mod)
+transformer = pickle.load(pickle_trans)
 
 #@app.route('/')
 def welcome():
     return "Welcome All"
 
 #@app.route('/predict',methods=["Get"])
-def predict_note_authentication(Age,Weight,Height,Neck,Chest,Abdomen,Hip,
-Thigh,Knee,Ankle,Biceps,Forearm,Wrist,BMI):
-    prediction=classifier.predict(Age,Weight,Height,Neck,Chest,Abdomen,Hip,
-    Thigh,Knee,Ankle,Biceps,Forearm,Wrist,BMI)
+def predict_note_authentication(X):
+    Y = transformer.transform(X)
+    prediction=classifier.predict(Y)
     print(prediction)
     return prediction
 
@@ -23,30 +24,28 @@ def main():
     st.title("Body Density Predictor")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+    <h2 style="color:white;text-align:center;">Streamlit Body Density Predictor ML App </h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
-    Age = st.text_input("Age","Type Here")
-    Weight = st.text_input("Weight","Type Here")
-    Height = st.text_input("Height","Type Here")
-    Neck = st.text_input("Neck","Type Here")
-    Chest = st.text_input("Chest","Type Here")
-    Abdomen = st.text_input("Abdomen","Type Here")
-    Hip = st.text_input("Hip","Type Here")
-    Thigh = st.text_input("Thigh","Type Here")
-    Knee = st.text_input("Knee","Type Here")
-    Ankle = st.text_input("Ankle","Type Here")
-    Biceps = st.text_input("Biceps","Type Here")
-    Forearm = st.text_input("Forearm","Type Here")
-    Wrist = st.text_input("Wrist","Type Here")
-    BMI = st.text_input("BMI","Type Here")
+    Age = st.text_input("Age","")
+    Neck = st.text_input("Neck","")
+    Abdomen = st.text_input("Abdomen","")
+    CtoH = st.text_input("Chest to Hip ratio","")
+    Thigh = st.text_input("Thigh","")
+    Knee = st.text_input("Knee","")
+    Ankle = st.text_input("Ankle","")
+    Biceps = st.text_input("Biceps","")
+    Forearm = st.text_input("Forearm","")
+    Wrist = st.text_input("Wrist","")
+    bmi = st.text_input("BMI","")
 
     result=""
     if st.button("Predict"):
-        result=predict_note_authentication(Age,Weight,Height,Neck,Chest,Abdomen,Hip,
-Thigh,Knee,Ankle,Biceps,Forearm,Wrist,BMI)
-    st.success('The output is {}'.format(result))
+        result=predict_note_authentication([[Age,Neck,Abdomen,CtoH,Thigh,Knee,Ankle,Biceps,Forearm,Wrist,bmi]])
+        
+    st.success('The density is {}'.format(result))
+
     if st.button("About"):
         st.text("Lets LEarn")
         st.text("Built with Streamlit")
